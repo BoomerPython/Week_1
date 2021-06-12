0f9bc34fc854eea472041298c11b9e0baec34071
BoomerPython <85760868+BoomerPython@users.noreply.github.com>
2021-06-11 23:32:38 -0500

    Add files via upload

---
+++ DSA_BoomerPython_Week1.py
@@ -0,0 +1,34 @@
+# This file provides an overview of commands 
+# & code demonstrated in week 1 of the DSA 5061 Python course.
+
+# Additional course materials depict the code shown here via 
+# the command line and also using a notebook.
+
+# The basics - math
+2+3
+
+# Storing results
+ans = 2 + 3
+print(ans)
+
+# Printing to the screen
+print("Boomer - Sooner!")
+
+# Storing a greeting
+
+greeting = "Boomer Sooner!"
+not_a_greeting = "Woo Pig!"
+print(greeting)
+
+#Checking a state and printing a greeting
+
+if ans > 3:
+  print(greeting)
+
+
+#Checking a state and printing an alternative
+
+if ans < 3:
+  print(greeting)
+else:
+  print(not_a_greeting)
